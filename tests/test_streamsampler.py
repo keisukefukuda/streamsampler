4fd8de3062bc30ac0cd3ee68cffe4863370cba6c
Keisuke Fukuda <keisukefukuda@gmail.com>
2013-11-18 00:00:54 -0700

    added several unittests

--- tests/test_streamsampler.py
+++ tests/test_streamsampler.py
@@ -1,9 +1,30 @@
 from streamsampler import StreamSampler
+import random
 import unittest
 
 class StreamSamplerTestCase(unittest.TestCase):
-    def test_ok(self):
-        self.assertTrue(True)
+    def setUp(self):
+        random.seed(0)
+        
+    def test_rand(self):
+        ss = StreamSampler(5, preserve=False)
+        for i in range(1000): ss.append(i)
+        self.assertEqual(list(ss),  [407, 998, 911, 517, 668])
+
+    def test_rand_preserved(self):
+        ss = StreamSampler(5)
+        for i in range(1000): ss.append(i)
+        self.assertEqual(list(ss),  [407, 517, 668, 911, 998])
+
+    def test_small(self):
+        ss = StreamSampler(5)
+        for i in range(5): ss.append(i)
+        self.assertEqual(list(ss), list(range(5)))
+
+    def test_small_2(self):
+        ss = StreamSampler(5)
+        ss.append_all(range(5))
+        self.assertEqual(list(ss), list(range(5)))
 
 def suite():
     loader = unittest.TestLoader()
